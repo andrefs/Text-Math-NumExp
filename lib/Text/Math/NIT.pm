df96f71a1e2617828f60ef1d1b61767f4d94a825
André Santos <andrefs@cpan.org>
2012-04-03 21:02:20 +0100

    migrated to Dist::Zilla

--- lib/Text/Math/NIT.pm
+++ lib/Text/Math/NIT.pm
@@ -1,39 +1,16 @@
-package Text::Math::NIT;
-
 use 5.006;
 use strict;
 use warnings;
+package Text::Math::NIT;
 
-=head1 NAME
-
-Text::Math::NIT - The great new Text::Math::NIT!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
+#ABSTRACT: Text::Math::NIT - Find Numbers In Text.
 
 use base 'Exporter';
-our @EXPORT = (qw/norm_numexp/);
+our @EXPORT = (qw/ norm_numexp
+				/);
 
-=head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Text::Math::NIT;
-
-    my $foo = Text::Math::NIT->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head1 DESCRIPTION
 
 =head1 SUBROUTINES/METHODS
 
@@ -55,63 +32,5 @@ sub norm_numexp {
 }
 
 
-=head1 AUTHOR
-
-Andre Santos, C<< <andrefs at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-text-math-nit at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Text-Math-NIT>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Text::Math::NIT
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Text-Math-NIT>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Text-Math-NIT>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Text-Math-NIT>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Text-Math-NIT/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2012 Andre Santos.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
 
 1;    # End of Text::Math::NIT
--- t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-    use_ok( 'Text::Math::NIT' ) || print "Bail out!\n";
-}
-
-diag( "Testing Text::Math::NIT $Text::Math::NIT::VERSION, Perl $], $^X" );
